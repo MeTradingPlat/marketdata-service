b9e13e1f504758880679e831ebe94262797d7988
Daniel Contreras <contrerasdaniel142@gmail.com>
2026-04-30 01:02:17 -0500

    chore: remove unused Service import

--- src/main/java/com/metradingplat/marketdata/domain/usecases/GestionarFundamentalsCUAdapter.java
+++ src/main/java/com/metradingplat/marketdata/domain/usecases/GestionarFundamentalsCUAdapter.java
@@ -7,7 +7,6 @@ import com.metradingplat.marketdata.infrastructure.output.external.finviz.Finviz
 import com.metradingplat.marketdata.infrastructure.output.external.tastytrade.TastyTradeService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
 
 import java.time.Instant;
 import java.time.LocalDate;
