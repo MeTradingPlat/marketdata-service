202f8cfca2b369cd81184002135eb62f9ac9070b
Daniel Contreras <contrerasdaniel142@gmail.com>
2026-04-30 00:52:25 -0500

    fix: remove duplicate @Service annotation causing NoUniqueBeanDefinitionException

--- src/main/java/com/metradingplat/marketdata/domain/usecases/GestionarFundamentalsCUAdapter.java
+++ src/main/java/com/metradingplat/marketdata/domain/usecases/GestionarFundamentalsCUAdapter.java
@@ -21,7 +21,6 @@ import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Slf4j
-@Service
 @RequiredArgsConstructor
 public class GestionarFundamentalsCUAdapter implements GestionarFundamentalsCUIntPort {
 
